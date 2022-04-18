e2e1e71d493a17a49560e5e42d6f0b1fa8462544
jigslipuff <81575620+jigslipuff@users.noreply.github.com>
2022-04-18 14:35:59 +0800

    Update scoreboard.py

--- snake game with classes/scoreboard.py
+++ snake game with classes/scoreboard.py
@@ -6,20 +6,36 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self. high_score = int(data.read())
+
         self.hideturtle()
         self.penup()
         self.goto(0, 310)
         self.color("white")
-        self.get_score = self.write(f"SCORE: {self.score}", align="center", font=("Arial", 12, "normal"))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"SCORE: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 12, "normal"))
 
     def add_score(self):
         self.score += 1
-        self.clear()
-        self.get_score = self.write(f"SCORE: {self.score}", align="center", font=("Arial", 12, "normal"))
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.get_score = self.write("GAME OVER", align="center", font=("Arial", 12, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+
+
+    # def game_over(self):
+        # self.goto(0,0)
+        # self.get_score = self.write("GAME OVER", align="center", font=("Arial", 12, "normal"))
 
     def increase_speed(self):
         if self.score >= 5:
